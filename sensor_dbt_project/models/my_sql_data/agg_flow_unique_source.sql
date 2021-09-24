2d30a649ca0af50cc2745fd6896d346d56782091
Stella Kibika <stellakibika@gmail.com>
2021-09-24 14:40:15 +0300

    airflow and dbt models

--- sensor_dbt_project/models/my_sql_data/agg_flow_unique_source.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-source_id,
-avg(avg_flow) AS average_traffic_flow
-  FROM {{ref('select_all_sample')}}
-  GROUP BY source_id
--- sensor_dbt_project/models/my_sql_data/select_all_sample.sql
+++
@@ -1,9 +0,0 @@
-with source_traffic_flow as (
-    select * from {{ source('sensor_db', 'sensordata') }}
-),
-
-final as(
-    select * from source_traffic_flow
-)
-
-select * from final
